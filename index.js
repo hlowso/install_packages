178a4836ba9ce5abd4f60aef97ca7840a8589567
hlowso <h.canderleigh@gmail.com>
2018-02-15 19:54:16 +0000

    added red, bold warning text

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-var chalk = require("chalk");
+const chalk = require("chalk");
 
-var message = "Hello " + chalk.yellow("World");
+const message = "Hello " + chalk.yellow("World");
+const error = chalk.bold.red;
+
 console.log(message);
+console.log(error('Uh-oh! It\'s an error!'));
